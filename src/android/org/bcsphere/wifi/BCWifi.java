4164e918a6aab1d38bec169922b84f39febf660b
lizhi <zhi.li@jumacc.com>
2014-03-21 13:56:02 +0800

    Update BCWifi.java

--- src/android/org/bcsphere/wifi/BCWifi.java
+++ src/android/org/bcsphere/wifi/BCWifi.java
@@ -24,36 +24,15 @@ public class BCWifi extends CordovaPlugin {
 		if (action.equals("getConnectedWifiInfo")) {
 			JSONObject obj = new JSONObject();
 			try {
-				JSONObject activity = new JSONObject();
-				activity.put("BSSID", wifiInfo.getBSSID());
-				activity.put("HiddenSSID", wifiInfo.getHiddenSSID());
-				activity.put("SSID", wifiInfo.getSSID());
-				activity.put("MacAddress", wifiInfo.getMacAddress());
-				activity.put("IpAddress", wifiInfo.getIpAddress());
-				activity.put("NetworkId", wifiInfo.getNetworkId());
-				activity.put("RSSI", wifiInfo.getRssi());
-				activity.put("LinkSpeed", wifiInfo.getLinkSpeed());
-				obj.put("activity", activity);
-
-				JSONArray available = new JSONArray();
-				for (ScanResult scanResult : wifiManager.getScanResults()) {
-					JSONObject ap = new JSONObject();
-					ap.put("BSSID", scanResult.BSSID);
-					ap.put("SSID", scanResult.SSID);
-					ap.put("frequency", scanResult.frequency);
-					ap.put("level", scanResult.level);
-					// netwrok.put("timestamp",
-					// String.valueOf(scanResult.timestamp));
-					ap.put("capabilities", scanResult.capabilities);
-					available.put(ap);
-				}
-				obj.put("available", available);
+				obj.put("BSSID", wifiInfo.getBSSID());
+				obj.put("SSID", wifiInfo.getSSID());
+				obj.put("MacAddress", wifiInfo.getMacAddress());
+				obj.put("IPAddress", wifiInfo.getIpAddress());
 			} catch (JSONException e) {
 				e.printStackTrace();
 				callbackContext.error("JSON Exception");
 			}
 			callbackContext.success(obj);
-
 		}
 		return true;
 	}
